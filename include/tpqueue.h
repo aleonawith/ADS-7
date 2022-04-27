ba44a0c7d3349dbb8299e4db366303cf2c7aacae
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-04-27 21:16:02 +0300

    Update tpqueue.h
    Смирнова Алена 21-ИСТ-2

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -10,60 +10,70 @@ class TPQueue {
      T value;
      ITEM* next;
      ITEM* prev;
-  };
+  }
   ITEM* head, * tail;
   TPQueue::ITEM* create(const T& value) {
      ITEM* item = new ITEM;
-     item -> value = value;
-     item -> next = nullptr;
-     item -> prev = nullptr;
+     item->value = value;
+     item->next = nullptr;
+     item->prev = nullptr;
      return item;
   }
-
+  
  public:
   TPQueue() {
-     head = tail = nullptr;
+     head = nullptr;
+     tail = nullptr;
   }
   ~TPQueue() {
-     while (head) {
-       pop();
-     }
+     while (head)
+     pop();
   }
   void push(const T& value) {
-     ITEM* t = head;
      ITEM* item = create(value);
-     while (t && t -> value.prior >= value.prior)
-         t = t -> next;
-     if (!t && head) {
-         tail -> next = item;
-         item -> prev = tail;
-         tail = item;
-     } else if (!t -> prev) {
-         head -> prev = item;
-         item -> next = head;
-         head = item;
-     } else if (!t && !head) {
-         head = tail = item;
+     ITEM* t = head;
+     while (t && t->value.prior >= value.prior)
+       t = t->next;
+     if (!t && !head) {
+       head = tail = item;
+     } else if (!t->prev) {
+       head->prev = item;
+       item->next = head;
+       head = item;
+     } else if (!t && head) {
+       tail->next = item;
+       item->prev = tail;
+       tail = item;
      } else {
-         t -> prev -> next = item;
-         item -> prev = t -> prev;
-         item -> next = t;
-         t -> prev = item;
+       t->prev->next = item;
+       item->prev = t->prev;
+       item->next = t;
+       t->prev = item;
+     }
+  }
+  bool isEmpty() {
+     if (!head || !tail) {
+       return 1;
+     } else {
+       return 0;
      }
   }
   T pop() {
-    assert(head);
-    ITEM *t = head -> next;
-    T value = head -> value;
-    delete head;
-    head = t;
-    return value;
+    if (isEmpty() == 1) {
+       throw std::string("Empty!");
+     }
+     ITEM* t = head->next;
+     if (t) {
+       t->prev = nullptr;
+     }
+     T value = head->value;
+     delete head;
+     head = t;
+     if (!head) {
+       tail = nullptr;
+      }
+      return value;
   }
-};
-
-struct SYM {
-  char ch;
-  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
