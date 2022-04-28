200831f882ad23e668f1a103922d7d6ea886d945
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-04-28 21:45:34 +0300

    Update tpqueue.h
    Смирнова Алена 21-ИСТ-2

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,54 +4,55 @@
 #include <string>
 
 template<typename T>
-class TPQueue {
+class TPQueue{
  private:
-  struct ITEM {
-     T value;
-     ITEM* next;
-  };
-  ITEM* first;
-  ITEM* last;
+struct ITEM {
+    T value;
+    ITEM* next;
+};
+ITEM* first;
+ITEM* last;
 
  public:
-  TPQueue() {
-     first = nullptr;
-     last = nullptr;
-  }
-  void push(T _val) {
-     ITEM* last = first;
-     if (last != nullptr) {
-       ITEM* t = new ITEM;
-       t -> value = _val;
-       if (first->value.prior < t -> value.prior) {
-         t -> next = last;
-         first = t;
-       }
-       while (last != nullptr) {
-         if ((last -> value.prior == t -> value.prior &&
-           (last -> next == nullptr
-           || last -> next -> value.prior < t -> value.prior)) ||
-           (last -> value.prior > t -> value.prior &&
-           ((last -> next != nullptr &&
-           last -> next -> value.prior < t -> value.prior)
-           || (last -> next == nullptr)))) {
-           t -> next = last -> next;
-           last -> next = t;
-           return;
-         }
-       last = last -> next;
-     } else {
-       last = new ITEM;
-       first = last;
-       last -> value = _val;
-       last -> next = nullptr;
-     }
-  }
-  T pop() {
-     ITEM* temp = first;
-     first = first -> next;
-     return (temp -> value);
-  }
+    TPQueue() {
+      first = nullptr;
+      last = nullptr;
+    }
+    void push(T _val) {
+      ITEM* tail = first;
+      if (tail != nullptr){
+        ITEM* t = new ITEM;
+        t -> value = _val;
+        if (first->value.prior < t -> value.prior) {
+          t -> next = last;
+          first = t;
+        }
+        while (last != nullptr) {
+          if ((last -> value.prior == t -> value.prior &&
+            (last -> next == nullptr
+            || last -> next -> value.prior < t -> value.prior)) ||
+            (last -> value.prior > t -> value.prior &&
+            ((last -> next != nullptr &&
+            last -> next -> value.prior < t -> value.prior)
+            || (last -> next == nullptr)))) {
+            t -> next = last -> next;
+            last -> next = t;
+            return;
+          }
+          last = last -> next;
+        }
+      } else {
+        last = new ITEM;
+        first = last;
+        last -> value = _val;
+        last -> next = nullptr;
+      }
+    }
+    T pop() {
+      ITEM* t = first;
+      first = first -> next;
+      return (t -> value);
+    }
 };
 
 struct SYM {
