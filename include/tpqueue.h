18c848a05dc46186ca1fe0d166d7bad145424bb7
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-04-30 11:13:56 +0300

    Update tpqueue.h
    Смирнова Алена 21-ИСТ-2

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,53 +6,62 @@
 template<typename T>
 class TPQueue{
  private:
-struct ITEM {
-    T value;
-    ITEM* next;
-};
-ITEM* first;
-ITEM* last;
+  struct ITEM {
+     T value;
+     ITEM* next;
+  };
+  ITEM* first, * last;
 
  public:
-    TPQueue() {
-      first = last = 0;
-    }
-    void push(T _val) {
-      ITEM* tail = first;
-      if (tail != nullptr) {
-        ITEM* t = new ITEM;
-        t -> value = _val;
-        if (first->value.prior < t -> value.prior) {
-          t -> next = last;
-          first = t;
-        }
-        while (last != nullptr) {
-          if ((last -> value.prior == t -> value.prior &&
-            (last -> next == nullptr
-            || last -> next -> value.prior < t -> value.prior)) ||
-            (last -> value.prior > t -> value.prior &&
-            ((last -> next != nullptr &&
-            last -> next -> value.prior < t -> value.prior)
-            || (last -> next == nullptr)))) {
-            t -> next = last -> next;
-            last -> next = t;
-            return;
-          }
-          last = last -> next;
-        }
-      } else {
-        last = new ITEM;
-        first = last;
-        last -> value = _val;
-        last -> next = nullptr;
-      }
-    }
-    T pop() {
-      ITEM* t = first;
-      first = first -> next;
-      return (t -> value);
-    }
+  TPQueue();
+  void push(T _val);
+  T pop();
 };
+
+template <typename T>
+TPQueue<T>::TPQueue() {
+  first = 0;
+  last = 0;
+}
+
+template <typename T>
+void TPQueue<T>::push(T _val) {
+  ITEM* last = first;
+  if (!last) {
+    last = new ITEM;
+    first = last;
+    last -> value = _val;
+    last -> next = nullptr;
+  } else {
+    ITEM* t = new ITEM;
+    t -> value = _val;
+    if (first -> value.prior < t -> value.prior) {
+      t -> next = last;
+      first = t;
+    }
+    while (last != nullptr) {
+      if ((last -> value.prior == t -> value.prior &&
+           (last -> next == nullptr ||
+            last -> next -> value.prior < t -> value.prior)) ||
+          (last -> value.prior > t -> value.prior &&
+           ((last -> next != nullptr &&
+             last -> next -> value.prior < t -> value.prior) ||
+            (last -> next == nullptr)))) {
+        t -> next = last -> next;
+        last -> next = t;
+        return;
+      }
+      last = last->next;
+    }
+  }
+}
+
+template <typename T>
+T TPQueue<T>::pop() {
+  ITEM* t = first;
+  first = first -> next;
+  return (t -> value);
+}
 
 struct SYM {
   char ch;
